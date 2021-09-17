0b70ffcfdbc3b4d6a924af6ade7759e725a0bd46
Ashik1704026 <39734793+Ashik1704026@users.noreply.github.com>
2021-09-17 20:55:10 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php echo "Hey man..." 
 
-// $con=mysqli_connect('localhost','root','','EMS') or die(mysqli_error());
-// if($con)
-//     echo "connected successfully";
-// else
-//     echo "not connected man...";
+$con=mysqli_connect('sql5.freesqldatabase.com','sql5437992','uRqlHhGhpf','sql5437992') or die(mysqli_error());
+if($con)
+    echo "connected successfully";
+else
+    echo "not connected man...";
 ?>
