bb7e3dc886108c4e98e0dfcca8042906817f648f
Ashik1704026 <39734793+Ashik1704026@users.noreply.github.com>
2021-09-17 20:53:21 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php echo "Hey man..." 
 
-$con=mysqli_connect('localhost','root','','EMS') or die(mysqli_error());
-if($con)
-    echo "connected successfully";
-else
-    echo "not connected man...";
+// $con=mysqli_connect('localhost','root','','EMS') or die(mysqli_error());
+// if($con)
+//     echo "connected successfully";
+// else
+//     echo "not connected man...";
 ?>
